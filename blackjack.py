b37cfc9f76b7f30b876eec320e142738ec4f5a27
Jeeglobs <dp.hudgins@hotmail.com>
2023-02-21 17:34:17 -0500

    moved file to new folder

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,37 @@
-# Write your blackjack game here.
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
+
+
+class Deck:
+    SUITS = ['♠️', '♣️', '♥️', '♦️']
+    RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+
+    def __init__(self):
+        self.cards = []
+
+        def add_cards(self):
+            for suit in SUITS:
+                for rank in RANKS:
+                    self.cards.append(Card(suit, rank))
+
+
+class Game:
+    def __init__(self, player, dealer, deck):
+        self.player = None
+        self.dealer = None
+        self.deck = None
+
+    def setup(self):
+        deck1 = Deck()
+        deck1.add_cards()
+        for card in deck1.cards:
+            print(card)
+
+
+new_game = Game()
+new_game.setup()
---
+++ scratch.py
@@ -0,0 +1,60 @@
+SUITS = ['♥️', '♣️', '♠️', '♦️']
+RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+
+
+class Card:
+    # parameters in parenthises must be wrritten out when Card is called (see line 23)
+    # if def __init__(self):
+    # then self.suit = None (or some other base value), same for self.rank
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
+
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+
+    def add_cards(self):
+        for suit in SUITS:
+            for rank in RANKS:
+                self.cards.append(Card(suit, rank))
+
+
+class Game:
+    def __init__(self):
+        self.player = None
+        self.dealer = None
+        self.deck = None
+        self.setup()
+        # calling the Game class's setup function
+
+    def setup(self):
+        self.deck = Deck()
+        # calls line 13, creates a deck
+        self.deck.add_cards()
+        # calls line 16, adds cards to the deck
+        for card in self.deck.cards:
+            print(card)
+            # calls the __str__ method for each card
+
+
+new_game = Game()
+# calls the Game class's init method in line 29
+# created the game and the deck with cards
+
+# TODO
+# make a player, like we did with Deck
+# make a dealer, also like we did with Deck
+# play
+# shuffle deck
+# deal cards
+# player's turn (hit/stay)
+# calculate score from cards in hand
+# dealer's turn
+# calculate score from cards in hand
+# who won/lost/busted/21
